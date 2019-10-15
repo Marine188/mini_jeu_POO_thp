7af99f904d205f9cf26cab336b954159cc8926ea
marine.calderon <marine.calderon@gmail.com>
2019-10-15 23:08:55 +0200

    supression des fichiers

--- lib/template_app.rb
+++
@@ -1,9 +0,0 @@
-#require 'rubygems'
-#-------Pour le scrapping-------
-#require 'nokogiri'
-#require 'open-uri'
-#-------Pour simuler un navigateur-------
-#require 'watir'
-#require 'launchy'
-#-------Pour les tests------
-#require 'pry'
